8c797749b91fb3e5d73f7eb710b75f5b8b1a1c74
alvyynm <alvyynm@gmail.com>
2023-04-10 14:28:30 +0300

    setup morgan

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import router from "./router";
+import morgan from "morgan";
 
 const app = express();
+app.use(morgan("dev"));
 
 app.get("/", (req, res, next) => {
   console.log("Hello from express");
