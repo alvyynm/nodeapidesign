53423dd438cb4318c95c2107459857caa2a05095
alvyynm <alvyynm@gmail.com>
2023-04-10 14:43:46 +0300

    add middleware for json and to parse incoming requests with URL-encoded payloads

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import morgan from "morgan";
 
 const app = express();
 app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res, next) => {
   console.log("Hello from express");
