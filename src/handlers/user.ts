e496fbfa80351ddc964375c41152ad32a79a4be7
alvyynm <alvyynm@gmail.com>
2023-04-11 04:46:47 +0300

    add logic for account creation and signin

---
+++ src/handlers/user.ts
@@ -0,0 +1,29 @@
+import prisma from "../utils/db";
+import { comparePasswords, createJWT, hashPassword } from "../utils/auth";
+
+export const createNewUser = async (req, res) => {
+  const user = await prisma.user.create({
+    data: {
+      username: req.body.username,
+      password: await hashPassword(req.body.password),
+    },
+  });
+
+  const token = createJWT(user);
+  res.json({ token: token });
+};
+
+export const signIn = async (req, res) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      username: req.body.username,
+    },
+  });
+
+  const isValid = await comparePasswords(req.body.password, user.password);
+
+  if (!isValid) {
+    res.status(401).json({ message: "Invalid credentials" });
+    return;
+  }
+};
