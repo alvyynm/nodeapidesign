688ade8e1cd919b8d88b73cfef898fc15bd4bd46
alvyynm <alvyynm@gmail.com>
2023-04-11 07:04:52 +0300

    add input validation

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { body, oneOf, validationResult } from "express-validator";
 
 const router = Router();
 
@@ -7,22 +8,86 @@ router.get("/product", (req, res, next) => {
   res.status(200).json({ message: "Success" });
 });
 router.get("/product/:id", () => {});
-router.put("/product/:id", () => {});
-router.post("/product", () => {});
+router.put("/product/:id", [body("name").isString()], (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    res.status(400).json({ errors: errors.array() });
+  }
+});
+router.post("/product", [body("name").isString()], (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    res.status(400).json({ errors: errors.array() });
+  }
+});
 router.delete("/product/:id", () => {});
 
 // Update routes
 router.get("/update", () => {});
 router.get("/update/:id", () => {});
-router.put("/update/:id", () => {});
-router.post("/update", () => {});
+router.put(
+  "/update/:id",
+  [
+    body("body").optional(),
+    body("title").optional(),
+    body("status").isIn(["IN_PROGRESS", "SHIPPED", "DEPRECATED"]).optional(),
+    body("version").optional(),
+  ],
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+    }
+  }
+);
+router.post(
+  "/update",
+  [body("title").exists().isString(), body("body").exists().isString()],
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+    }
+  }
+);
 router.delete("/update/:id", () => {});
 
 // Updatepoint routes
 router.get("/updatepoint", () => {});
 router.get("/updatepoint/:id", () => {});
-router.put("/updatepoint/:id", () => {});
-router.post("/updatepoint", () => {});
+router.put(
+  "/updatepoint/:id",
+  [
+    body("name").optional().isString(),
+    body("description").optional().isString(),
+  ],
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+    }
+  }
+);
+router.post(
+  "/updatepoint",
+  [
+    body("name").isString(),
+    body("description").isString(),
+    body("updateId").exists().isString(),
+  ],
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+    }
+  }
+);
 router.delete("/updatepoint/:id", () => {});
 
 export default router;
