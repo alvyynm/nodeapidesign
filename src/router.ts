49cf2a1cf299569d6e4dd7f172a2f0f35a66b658
alvyynm <alvyynm@gmail.com>
2023-04-12 06:53:17 +0300

    check productId for validation

--- src/router.ts
+++ src/router.ts
@@ -44,7 +44,11 @@ router.put(
 );
 router.post(
   "/update",
-  [body("title").exists().isString(), body("body").exists().isString()],
+  [
+    body("title").exists().isString(),
+    body("body").exists().isString(),
+    body("productId").exists().isString(),
+  ],
   handleInputErrors,
   (req, res, next) => {}
 );
