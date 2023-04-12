13bbc00937afdfaf4f928225e190b4d131131ec9
alvyynm <alvyynm@gmail.com>
2023-04-12 07:27:03 +0300

    create controller for updatepoint model

---
+++ src/handlers/updatepoint.ts
@@ -0,0 +1,16 @@
+import prisma from "../utils/db";
+
+// GET update points
+export const getUpdatepoints = async (req, res, next) => {};
+
+// GET one updatepoint
+export const getOneUpdatepoint = async (req, res, next) => {};
+
+// UPDATE updatepoint
+export const updateUpdatepoint = async (req, res, next) => {};
+
+// CREATE updatepoint
+export const createUpdatepoint = async (req, res, next) => {};
+
+// DELETE updatepoint
+export const deleteUpdatepoint = async (req, res, next) => {};
